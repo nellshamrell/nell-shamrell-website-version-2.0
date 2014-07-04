8885f995998842de0069ee4c5ef1914000d07b8f
Nell Shamrell <nshamrell@bluebox.net>
2014-07-04 16:54:47 -0700

    bare bones skeleton routes

---
+++ web.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get '/' do
+  'This is the Home Page'
+end
+
+get '/coding' do
+  'This is the coding page'
+end
+
+get '/speaking' do
+  'This is the speaking page'
+end
+
+get '/contact' do
+  'This is the contact page'
+end
